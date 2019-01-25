8711fcb209fd40a83748bc1bfb1e8f8cda6e9754
Muhammad Umar Bilal <umerchohan8@gmail.com>
2019-01-25 11:49:58 +0500

    second commit

--- BlogPost/BlogPost/settings.py
+++ BlogPost/BlogPost/settings.py
@@ -146,5 +146,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'umerchohan8@gmail.com'
-EMAIL_HOST_PASSWORD = '2013pakistan'
+EMAIL_HOST_USER = 'YOUR_EMAIL'
+EMAIL_HOST_PASSWORD = 'YOUR_PASSWORD'
